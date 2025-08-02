734cf5d0f55300dd2c2a6c797c41cd85ca04beae
GB-BOT-MD <gbbotmd@gmail.com>
2025-08-01 23:57:17 -0300

    config.js

--- config.js
+++ config.js
@@ -3,13 +3,13 @@ import chalk from 'chalk'
 import { fileURLToPath } from 'url' 
 
 global.owner = [
-  ['5491168352204', 'FG', true],
-  ['5493794297363'],
-  ['59172945992']
+  ['59896172320', 'FG', true],
+  ['59896449730'],
+  ['59894243071']
 ] //Numeros de owner 
 
 global.mods = [''] 
-global.prems = ['50489079501', '573143917092']
+global.prems = ['59896172320', '59896449730']
 global.botNumber = [''] 
 global.APIs = { // API Prefix
   // name: 'https://website' 
